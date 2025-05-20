4baab9e5101820a07648ba467afd6181ec7544b8
Nancy-Pauline <nancypaulinenancy@gmail.com>
2025-05-20 12:52:55 +0300

    added double variable

--- Task1/main.c
+++ Task1/main.c
@@ -5,8 +5,10 @@ int main()
 {
     int age = 20;
     float height = 5.9;
+    double distance = 12345.6789;
 
     printf("The age is %d\n", age);
     printf("The height is %f\n", height);
+    printf("The distance is %lf\n", distance);
     return 0;
 }
