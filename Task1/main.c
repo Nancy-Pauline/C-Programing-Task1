f68eb8bdc174e85ccdf643d8eb1093c1626e19d6
Nancy-Pauline <nancypaulinenancy@gmail.com>
2025-05-20 12:59:50 +0300

    added char variable

--- Task1/main.c
+++ Task1/main.c
@@ -6,9 +6,11 @@ int main()
     int age = 20;
     float height = 5.9;
     double distance = 12345.6789;
+    char initial = 'A';
 
     printf("The age is %d\n", age);
     printf("The height is %f\n", height);
     printf("The distance is %lf\n", distance);
+    printf("The initial is %c\n", initial);
     return 0;
 }
